78a0bef793b47c13e74c72aade9ba9bc58ac32e5
BillieHawk <benl6252@gmail.com>
2023-10-28 14:57:11 -0400

    formated csv

--- Retrieving_course_info.py
+++ Retrieving_course_info.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+def append_to_csv(data, csv_file):
+    with open(csv_file, 'a', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(data)
+
 def retrieve_course(session_term: str, subject_abbr: str, catalog_number: str) -> None:
     session = requests.Session()
 
@@ -24,27 +29,32 @@ def retrieve_course(session_term: str, subject_abbr: str, catalog_number: str) -
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        # Specify the CSV file name
-        csv_file = "output.csv"
+        # Find all <tr> tags
+        tr_tags = soup.find_all('tr')
 
-        # Open the CSV file for writing
-        with open(csv_file, 'w', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-
-            # Find all <td> tags
-            td_tags = soup.find_all('td')
-
-            if not td_tags:
-                print("No course information found.")
-            else:
+        if not tr_tags:
+            print("No course information found.")
+        else:
+            # Specify the CSV file name
+            csv_file = "output.csv"
+            
+            for tr in tr_tags:
+                data = [subject_abbr, catalog_number]  # Add subject abbreviation and catalog number as the first two columns
+                td_tags = tr.find_all('td')
                 for td in td_tags:
-                    # Write the text inside the <td> tag to the CSV file
-                    csv_writer.writerow([td.text])
+                    strong_tag = td.find('strong')
+                    if strong_tag is None:  # Ignore <td> elements containing <strong> tags
+                        text = td.text.strip()  # Remove leading/trailing whitespace
+                        if text:  # Check if the text is not empty
+                            data.append(text)
+                # Append the data to the CSV file for each <tr> tag
+                append_to_csv(data, csv_file)
 
-        print(f"CSV file '{csv_file}' created.")
+            print("Data appended to CSV file.")
+
     else:
         print("Failed to retrieve course information. Please check your inputs and the website.")
 
+# Example usage:
 retrieve_course("2024 Spring", "COMP", "210")
-retrieve_course("2024 Spring", "COMP", "110")
-retrieve_course("2024 Spring", "MATH", "130")
+retrieve_course("2024 Spring", "MATH", "110")
