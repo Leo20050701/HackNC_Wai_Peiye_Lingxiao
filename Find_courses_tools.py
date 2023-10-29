15653fb9ba1d73dd381ffce9bc28170b7a835830
Lingxiao Han <hanlingx@unc.edu>
2023-10-29 00:19:26 -0400

    add course searching features and main.py

--- Find_courses_tools.py
+++ Find_courses_tools.py
@@ -1,6 +1,7 @@
 """Functions to find desired courses."""
 from config import *
 import csv
+import re
 
 def read_csv(csv_fileName) -> list[dict]:
     """Read .csv file and store the data into list[dict]."""
@@ -10,14 +11,64 @@ def read_csv(csv_fileName) -> list[dict]:
         for row in reader:
             data.append(row)
         
-    return data
+    if data:
+        return data
+    return
 
 
-def find_subject(course_data: list, subject_abbr: str, subject_number: str) -> list:
+def find_subject(course_data: list[dict], subject_abbr: str, subject_number: str) -> list[dict]:
     """Find course with chosen subject."""
     new_data: list[dict] = list()
     for course in course_data:
         if course["Name"] == subject_abbr and course["Number"] == subject_number:
             new_data.append(course)
+    
+    if new_data:
+        return new_data
+    return
+
+
+def find_time_between(course_data: list[dict], earliest_time: str, latest_time: str) -> list[dict]:
+    """Filter course between time A and B."""
+    new_data: list[dict] = list()
+    start_time: float = 0.0
+    end_time: float = 0.0
+    #Process the begin and end time, from str to float, from 12h to 24h
+    if "PM" in earliest_time and "12" not in earliest_time:
+        earliest_time = float(earliest_time[0:2]) + 12.0 + float(earliest_time[3:5]) / 60
+    else:
+        earliest_time = float(earliest_time[0:2]) + float(earliest_time[3:5]) / 60
+
+    if "PM" in latest_time and "12" not in latest_time:
+        latest_time = float(latest_time[0:2]) + 12.0 + float(latest_time[3:5]) / 60
+    else:
+        latest_time = float(latest_time[0:2]) + float(latest_time[3:5]) / 60
+
+
+    for course in course_data:
+        if course["Time"] == None:
+            pass
+        else:
+            start_time = re.search(r"^[A-Z]+ ([0-9][0-9]:[0-9][0-9] (PM|AM))", course["Time"])
+            end_time = re.search(r"([0-9][0-9]:[0-9][0-9] (PM|AM))$", course["Time"])
+            if start_time and end_time:
+                start_time = start_time.group(1)
+                end_time = end_time.group(1)
+
+            if start_time and end_time:
+                #Process the start and end time, from str to float, from 12h to 24h
+                if "PM" in start_time and "12" not in start_time:
+                    start_time = float(start_time[0:2]) + 12.0 + float(start_time[3:5]) / 60
+                else:
+                    start_time = float(start_time[0:2]) + float(start_time[3:5]) / 60
+            
+                if "PM" in end_time and "12" not in end_time:
+                    end_time = float(end_time[0:2]) + 12.0 + float(end_time[3:5]) / 60
+                else:
+                    end_time = float(end_time[0:2]) + float(end_time[3:5]) / 60
+
+
+                if earliest_time <= start_time and latest_time >= end_time:
+                    new_data.append(course)
 
     return new_data
--- main.py
+++ main.py
@@ -6,6 +6,17 @@ import csv
 
 csv_fileName = "output.csv"
 
+course_criteria: dict[str: str] = {
+    "subject": "MATH",
+    "number": "381",
+    "start_Time": "08:00",
+    "end_Time": "13:00",
+    "Date": "MTWTHF"
+}
+
 if __name__ == "__main__":
-    print(find_subject(read_csv(csv_fileName), "MATH", "381"))
+    #Testing...
+    search_result = find_subject(read_csv(csv_fileName), course_criteria["subject"], course_criteria["number"])
+    search_result = find_time_between(search_result, course_criteria["start_Time"], course_criteria["end_Time"])
+    print(search_result)
     #write_dict_to_csv(retrieve_course("2024 Spring", "IDS", "101"), csv_fileName)
