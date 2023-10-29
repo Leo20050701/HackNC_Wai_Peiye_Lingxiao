393564efc4f9b32623f66a14b247c42d3d16de0d
Lingxiao Han <hanlingx@unc.edu>
2023-10-29 02:11:33 -0400

    updated features

--- main.py
+++ main.py
@@ -23,5 +23,11 @@ if __name__ == "__main__":
     #Testing...
     #search_result = find_subject(read_csv(csv_fileName), course_criteria["subject"], course_criteria["number"])
     #search_result = find_time_between(search_result, course_criteria["start_Time"], course_criteria["end_Time"])
-    print(find_course(csv_fileName, course_criteria[0]["subject"], course_criteria[0]["number"], course_criteria[0]["start_Time"],course_criteria[0]["end_Time"]))
+    #print(find_course_date(find_course(csv_fileName, course_criteria[0]["subject"], course_criteria[0]["number"], course_criteria[0]["start_Time"],course_criteria[0]["end_Time"])))
     #write_dict_to_csv(retrieve_course("2024 Spring", "IDS", "101"), csv_fileName)
+    
+    # Main script
+
+    # Sample course data
+    #print(find_course_date(test_data))
+    # Test the function
