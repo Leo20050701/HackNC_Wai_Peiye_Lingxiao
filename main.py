1f044f6bac72700cc9c17b3e767c3797f0cd09f3
Lingxiao Han <hanlingx@unc.edu>
2023-10-31 13:02:48 -0400

    updated

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     session_term = "2024 Spring"
 
     #Subject Abbr
-    courses_abbr = ["ECON", "CHEM", "COMP", "CHEM", "BUSI"]
+    courses_abbr = ["MATH", "CHEM", "COMP", "BUSI", "ECON"]
 
     #Course Number
-    course_number = ["410", "101", "210", "101L", "100"]
+    course_number = ["381", "101", "210", "100", "410"]
 
     #Time range
-    earliest_time = "10:00 AM"
+    earliest_time = "09:00 AM"
     latest_time = "06:00 PM"
     
     # This is to check wether the course that we search already in the .csv file, if not, retrieve it online.
@@ -31,6 +31,10 @@ if __name__ == "__main__":
     result = find_valid_schedule(csv_fileName, courses_abbr, course_number, earliest_time, latest_time)
     
     for course_dict, schedule_sequence in zip(result, range(len(result))):
+        credit_hour = 0
         print(f"\nSchedule {schedule_sequence+1}")
         for course in course_dict:
-            print(f"{course['Name']} - {course['Number']}   {course['Time']}")
+            credit_hour += float(course["Hours"])
+            print(f"{course['Name']}{course['Number']} \t{course['Period']} \t {course['Time']} \t Seats: {course['Seats']} \t{course['Instructor']}")
+        
+        print(f"Credit Hour: {credit_hour}")
